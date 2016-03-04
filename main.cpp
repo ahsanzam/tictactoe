ce4db1c2d42da2006112b4758ba6afe03a1173b1
Ahsan Zaman <Zaman@guest-wireless-207-151-061-055.usc.edu>
2016-03-04 12:56:47 -0800

    Added MoveGen function
    Still in progress

--- main.cpp
+++ main.cpp
@@ -38,6 +38,7 @@ vector<Square*> set_up_board();
 bool play_game_mfirst(vector<Square*> &s);
 bool play_game_pfirst(vector<Square*> &s);
 int move_gen(vector<Square*> &s);
+int move_gen(vector<Square*> &s, int &state);
 int check_win(vector<Square*> &s);
 bool is_error(int input, vector<Square*> &s);
 bool is_number(string input);
@@ -75,8 +76,7 @@ int main(){
 }
 
 //prints out values for the board
-void show_board(vector<Square*> &s)
-{
+void show_board(vector<Square*> &s){
 	for(int i=1; i<10; i++){
 		s[i]->print();
 		if(!(i%3)){cout << "\n__|___|___|\n";}
@@ -84,8 +84,7 @@ void show_board(vector<Square*> &s)
 }
 
 //pushes in initial values for squares
-vector<Square*> set_up_board()
-{
+vector<Square*> set_up_board(){
 	vector<Square*> v;
 	v.push_back(NULL);
 	for(int i=1; i<10; i++){
@@ -98,19 +97,19 @@ vector<Square*> set_up_board()
 //enters game loop with player moving first
 bool play_game_pfirst(vector<Square*> &s)
 {
+	int state = 1;
 	bool at_end = false;
 	while(!at_end){
 		string input;
 		int index;
 		bool error = true; 
 		cout << "Pick a square: " ;
-
 		//check for error in input
 		while(error){
 			getline(cin, input);
 			if(!(is_number(input))) error = true;
 			else if(is_error(atoi(input.c_str()), s)) error = true;
-			else{
+			else{ 
 				error = false;
 				index = atoi(input.c_str());
 			} 
@@ -119,17 +118,20 @@ bool play_game_pfirst(vector<Square*> &s)
 		s[index]->set_val(X);
 		show_board(s);
 		if(check_win(s) != 0) at_end = true;
+		state++;
 
 		//machine's turn
 		if(!at_end){
 			cout << "My turn!\n";
-			index = move_gen(s);
+			if(state==2) index = move_gen(s, state);
+			else index = move_gen(s);
 			cout << "I pick square "<< index << endl;
 			s[index]->set_val(O);
 			show_board(s);
 			if(check_win(s) != 0) 
 				at_end = true;
 		}
+		state++;
 	}
 	if(check_win(s)==O) return true;
 	else return false;
@@ -137,6 +139,7 @@ bool play_game_pfirst(vector<Square*> &s)
 //enters game loop with machine moving first
 bool play_game_mfirst(vector<Square*> &s)
 {
+	int state = 1;
 	bool at_end = false;
 	while(!at_end){
 		string input;
@@ -144,12 +147,15 @@ bool play_game_mfirst(vector<Square*> &s)
 		bool error = true; 
 		//machine's turn
 		cout << "My turn!\n";
-		index = move_gen(s);
+		if(state==1) index = move_gen(s, state);
+		else index = move_gen(s);
 		cout << "I pick square "<< index << endl;
 		s[index]->set_val(O);
 		show_board(s);
 		if(check_win(s) != 0) 
 			at_end = true;
+		state++; 
+
 		//player's turn
 		if(!at_end){
 			cout << "Your turn. Pick a square: " ;
@@ -168,17 +174,55 @@ bool play_game_mfirst(vector<Square*> &s)
 			show_board(s);
 			if(check_win(s) != 0) at_end = true;
 		}
+		state++;
 	}
 	if(check_win(s)==O) return true;
 	else return false;
 }
-
-//allows computer to generate moves 
-int move_gen(vector<Square*> &s)
+//allows computer to generate moves for first move
+int move_gen(vector<Square*> &s, int &state)
 {
-	int x;
-	cin >> x ;
-	return x;
+	if(state==1) return 1;
+	else if(s[5]->val()!=X) return 5;
+	else return 1;
+}
+//allows computer to generate moves for move after first
+int move_gen(vector<Square*> &s){
+	//after first move
+	if(s[1]->val()==s[2]->val() && s[3]->val()>0) return 3; //1
+	if(s[4]->val()==s[5]->val() && s[6]->val()>0) return 6; //2
+	if(s[7]->val()==s[8]->val() && s[9]->val()>0) return 9; //3
+	if(s[1]->val()==s[5]->val() && s[9]->val()>0) return 9; //4
+	if(s[3]->val()==s[5]->val() && s[7]->val()>0) return 7; //5
+	if(s[2]->val()==s[5]->val() && s[8]->val()>0) return 8; //6
+	if(s[7]->val()==s[5]->val() && s[3]->val()>0) return 3; //7
+	if(s[8]->val()==s[5]->val() && s[2]->val()>0) return 2; //8
+	if(s[6]->val()==s[5]->val() && s[4]->val()>0) return 4; //9
+	if(s[9]->val()==s[5]->val() && s[1]->val()>0) return 1; //10
+	if(s[3]->val()==s[2]->val() && s[1]->val()>0) return 1; //11
+	if(s[9]->val()==s[8]->val() && s[7]->val()>0) return 7; //12
+	if(s[1]->val()==s[9]->val() && s[5]->val()>0) return 5; //13
+	if(s[3]->val()==s[7]->val() && s[5]->val()>0) return 5; //14
+	if(s[1]->val()==s[3]->val() && s[2]->val()>0) return 2; //15
+	if(s[4]->val()==s[6]->val() && s[5]->val()>0) return 5; //16
+	if(s[7]->val()==s[9]->val() && s[8]->val()>0) return 8; //17
+	if(s[1]->val()==s[7]->val() && s[4]->val()>0) return 4; //18
+	if(s[2]->val()==s[8]->val() && s[5]->val()>0) return 5; //19
+	if(s[3]->val()==s[9]->val() && s[6]->val()>0) return 6; //20
+	else{
+		cout << "MISSED ONE!" << endl;
+		return 2;
+	}
+
+
+
+	/*if(s[1]->val()==s[5]->val() && s[9]->val()!=O) return 9; //13
+	if(s[1]->val()==s[5]->val() && s[9]->val()!=O) return 9; //14
+	if(s[1]->val()==s[5]->val() && s[9]->val()!=O) return 9; //15
+	if(s[1]->val()==s[5]->val() && s[9]->val()!=O) return 9; //16
+	if(s[1]->val()==s[5]->val() && s[9]->val()!=O) return 9; //17
+	*/
+
 }
 
 //checks for end state
